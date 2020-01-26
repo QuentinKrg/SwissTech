051fa3be52ab1872877fe2ec1a24d38a8c6ce4f9
QuentinKrg <38885215+QuentinKrg@users.noreply.github.com>
2020-01-26 20:16:52 +0100

    Redoo du projet angular

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-swisstech'`, () => {
+  it(`should have as title 'frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-swisstech');
+    expect(app.title).toEqual('frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-swisstech app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('frontend app is running!');
   });
 });
