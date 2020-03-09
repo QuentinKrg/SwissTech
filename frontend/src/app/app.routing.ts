8e6788054b91cacc921f663eeb3966e085e2f974
wmeisen <43538554+wmeisen@users.noreply.github.com>
2020-03-09 23:47:56 +0100

    sidebar test

--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -5,7 +5,6 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { AuthGuard } from './helpers/auth.guard';
 import { MyProfileComponent } from './components/my-profile/my-profile.component';
-import { MyProfileSidebarComponent } from './components/my-profile/my-profile-sidebar/my-profile-sidebar.component';
 
 
 
@@ -14,7 +13,7 @@ const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'myprofile', component: MyProfileComponent, canActivate: [AuthGuard] },
-    { path: '', component:MyProfileSidebarComponent },
+   
 
     // Autrement redirige to home
     { path: '**', redirectTo: '' }
