435254cc1547ff57864505f46cb40f849a55a4ba
wmeisen <43538554+wmeisen@users.noreply.github.com>
2020-04-02 11:57:34 +0200

    Menu Admin Hidden

--- frontend/src/app/components/my-profile/sidebar/sidebar.component.ts
+++ frontend/src/app/components/my-profile/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+  currentUser: User;
+  currentUserRole: string;
 
-  constructor() { }
+  constructor(
+    private authenticationService: AuthenticationService
+  ) {
+    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+   }
 
   ngOnInit() {
+    this.currentUserRole = this.currentUser.role;
+    console.log(this.currentUserRole);
+    
   }
 
 }
