3f36b7db45237d1e9788cff438da5e5516a6b7a5
QuentinKrg <38885215+QuentinKrg@users.noreply.github.com>
2020-05-30 22:26:55 +0200

    Impression

--- frontend/src/app/components/my-profile/my-orders/print/invoice/invoice.component.ts
+++ frontend/src/app/components/my-profile/my-orders/print/invoice/invoice.component.ts
@@ -3,6 +3,7 @@ import { OrdersService } from 'src/app/services/orders.service';
 import { ActivatedRoute } from '@angular/router';
 import { Order } from 'src/app/models/order';
 import { environment } from 'src/environments/environment';
+import { PrintService } from 'src/app/services/print.service';
 
 @Component({
   selector: 'app-invoice',
@@ -14,17 +15,32 @@ export class InvoiceComponent implements OnInit {
   orderId: number;
   orderDetails: Order[];
   imageUrl: string;
+  orderInfos: Order;
   
   constructor(
     private _orderService: OrdersService,
-    private _route: ActivatedRoute
+    private _route: ActivatedRoute,
+    private _printService: PrintService
   ) { 
     this.orderId = this._route.snapshot.params['id'];
     this.imageUrl = environment.imageDirectory;
   }
 
   ngOnInit() {
-    this._orderService.getOrderDetailsByOrderID(this.orderId).subscribe(data => this.orderDetails = data);
+    this.orderInfos = JSON.parse(localStorage.getItem('order'));
+    
+    this.getInvoiceDetails(this.orderId).then(() => {this._printService.onDataReady()});
+  }
+
+  getInvoiceDetails(id: number) {
+    
+    this._orderService.getOrderDetailsByOrderID(id).subscribe(data => {
+      this.orderDetails = data;
+    });
+    
+    return new Promise((resolve) => {
+      setTimeout(() => resolve(this.orderInfos), 1000)
+    });
   }
 
 }
--- frontend/src/app/services/print.service.ts
+++ frontend/src/app/services/print.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Order } from '../models/order';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PrintService {
 
-  constructor() { }
+  constructor(private _router: Router ) { }
+  isPrinting = false;
+
+  printDocument(id: number) {
+    this.isPrinting = true;
+    this._router.navigate(['/print/invoice/'+id]);
+  }
+
+  onDataReady() {
+    setTimeout(() => {
+      window.print();
+      this.isPrinting = false;
+      localStorage.removeItem('order');
+      this._router.navigate(['/myprofile/myorders']);
+    });
+  }
 }
